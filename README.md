f340fdf8e76ebe090bced853565dbdacfc14eac9
gsomer <gasomer75@hotmail.com>
2022-03-06 00:49:49 -0800

    add questions and readme layout

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-# ReadMe.md File Generator
+# Professional ReadMe File Generator
 
-## Project Goal
+## Table Of Contents
 
-    - To use the Command Line with node.js and npm packages to create a JavaScript application that accepts user input on the Command Line and returns a professional ReadMe.md file based on the input received.
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Link](#link)
+- [Contributions](#contributions)
+- [Testing](#testing)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+- This application was designed to simplify creating ReadMe files by using a command line interface where question prompts allow user input into fields that generally appear in a professional ReadMe.md file. The input is collected and then used to complete the template and generate a ReadMe file.
+- The template used contains the main sections that are typically included in a ReadMe file. It's basic design is what makes it simple and appealing. However, there may be sections that a user wishes to include in their ReadMe file that are not part of this template.
+
+## Installation
+
+## Usage
+
+![Command Line Prompts](./images/cl-screenshot.png "Question Prompts")
+
+![ReadMe File](./images/rm-screenshot.png "Generated ReadMe File")
+
+## Link
+
+## Contributions
+
+## Testing
+
+## License
+
+## Questions
--- index.js
+++ index.js
@@ -2,14 +2,157 @@
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub user name: ',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub user name to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter an email address for people to contact you about this project: ',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Please enter an email address to continue!');
+        return false;
+      }
+    }
+  },
+  {   
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project: ',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter a title to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project: ',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please enter a project description to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'screenshot',
+    message: 'Enter the file name of your screenshot (file must be in /dist/images/ folder): ',
+    default: ''
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Provide guidelines for others to contribute to your project: ',
+    validate: contributionsInput => {
+      if (contributionsInput) {
+        return true;
+      } else {
+        console.log('Please give contribution instructions to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide instructions for installation of your project: ',
+    validate: installationInput => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log('Please give installation requirements to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: "Enter the url to your deployed application.(Press Enter to bypass): ",
+    default: 'No deployment.'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage instructions and examples: ',
+    validate: usageInput => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log('Please give usage instructions to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'testing',
+    message: 'Provide any testing guidelines for this application: ',
+    validate: testingInput => {
+      if (testingInput) {
+        return true;
+      } else {
+        console.log('Please give testing guidelines to continue!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'confirmLicense',
+    message: 'Would you like to include a license for your project?',
+    default: true
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license for your project: ',
+    choices: ['Apache-2.0', 'MIT', 'GPL-3.0', 'MPL-2.0', 'NONE'],
+    when: ({confirmLicense}) => {
+      if (confirmLicense) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(answers => {
+      console.log(answers);
+    })
+}
 
 // Function call to initialize app
 init();
-
